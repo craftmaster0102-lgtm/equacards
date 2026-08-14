f590f156ec6bbab76d9bf819d12ceb6ad6f3a22f
craftmaster0102-lgtm <craftmaster0102@gmail.com>
2026-08-14 10:55:44 +0530

    mother fucker

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 app.use(express.json());
 
 // ==========================================
